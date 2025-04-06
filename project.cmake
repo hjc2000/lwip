3ac8df2c9cdea6b1c631766cf8ab8d09b2bfeaaf
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-06 10:20:44 +0800

    project.cmake

---
+++ project.cmake
@@ -0,0 +1,8 @@
+add_library(${ProjectName})
+target_import_src(${ProjectName})
+target_import_freertos(${ProjectName} PUBLIC)
+target_import_bsp_interface(${ProjectName} PUBLIC)
+# target_import_freertos(${ProjectName} PUBLIC)
+# target_import_base(${ProjectName} PUBLIC)
+# target_import_task(${ProjectName} PUBLIC)
+# target_import_stm32h743iit6_hal(${ProjectName} PRIVATE)
