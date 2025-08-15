310f07c35155d2b374adb7119a4c6a60c5414ec5
hjc2000 <hjc.666@foxmail.com>
2025-08-15 13:22:23 +0800

    统一代码格式：将#pragma region替换为/* #region */注释风格

--- include/arch/sys_arch.h
+++ include/arch/sys_arch.h
@@ -21,11 +21,11 @@ void sys_arch_msleep(u32_t delay_ms);
 typedef u32_t sys_prot_t;
 #endif /* SYS_LIGHTWEIGHT_PROT */
 
-#pragma region 互斥量
+/* #region 互斥量 */
 
 struct _sys_mut
 {
-    void *mut;
+	void *mut;
 };
 
 typedef struct _sys_mut sys_mutex_t;
@@ -33,13 +33,13 @@ typedef struct _sys_mut sys_mutex_t;
 #define sys_mutex_valid(mutex) (((mutex) != NULL) && sys_mutex_valid_val(*(mutex)))
 #define sys_mutex_set_invalid(mutex) ((mutex)->mut = NULL)
 
-#pragma endregion
+/* #endregion */
 
-#pragma region 信号量
+/* #region 信号量 */
 
 struct _sys_sem
 {
-    void *sem;
+	void *sem;
 };
 
 typedef struct _sys_sem sys_sem_t;
@@ -47,13 +47,13 @@ typedef struct _sys_sem sys_sem_t;
 #define sys_sem_valid(sema) (((sema) != NULL) && sys_sem_valid_val(*(sema)))
 #define sys_sem_set_invalid(sema) ((sema)->sem = NULL)
 
-#pragma endregion
+/* #endregion */
 
-#pragma region 邮箱
+/* #region 邮箱 */
 
 struct _sys_mbox
 {
-    void *mbx;
+	void *mbx;
 };
 
 typedef struct _sys_mbox sys_mbox_t;
@@ -61,26 +61,26 @@ typedef struct _sys_mbox sys_mbox_t;
 #define sys_mbox_valid(mbox) (((mbox) != NULL) && sys_mbox_valid_val(*(mbox)))
 #define sys_mbox_set_invalid(mbox) ((mbox)->mbx = NULL)
 
-#pragma endregion
+/* #endregion */
 
-#pragma region 线程
+/* #region 线程 */
 
 struct _sys_thread
 {
-    void *thread_handle;
+	void *thread_handle;
 };
 
 typedef struct _sys_thread sys_thread_t;
 
-#pragma endregion
+/* #endregion */
 
 #if LWIP_NETCONN_SEM_PER_THREAD
 sys_sem_t *sys_arch_netconn_sem_get(void);
 void sys_arch_netconn_sem_alloc(void);
 void sys_arch_netconn_sem_free(void);
-#define LWIP_NETCONN_THREAD_SEM_GET() sys_arch_netconn_sem_get()
-#define LWIP_NETCONN_THREAD_SEM_ALLOC() sys_arch_netconn_sem_alloc()
-#define LWIP_NETCONN_THREAD_SEM_FREE() sys_arch_netconn_sem_free()
+	#define LWIP_NETCONN_THREAD_SEM_GET() sys_arch_netconn_sem_get()
+	#define LWIP_NETCONN_THREAD_SEM_ALLOC() sys_arch_netconn_sem_alloc()
+	#define LWIP_NETCONN_THREAD_SEM_FREE() sys_arch_netconn_sem_free()
 #endif /* LWIP_NETCONN_SEM_PER_THREAD */
 
 #endif /* LWIP_ARCH_SYS_ARCH_H */
